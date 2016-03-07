00c275250e03302a788e8af25b086e7fedb68fa8
Sean Chu <chussean@gmail.com>
2016-03-06 20:49:36 -0500

    oops

--- HW 04/test.js
+++ HW 04/test.js
@@ -1,11 +1,38 @@
 var pic = document.getElementById("vimg");
 var clearbutton = document.getElementById("clear");
 var start = document.getElementById("start");
+var startbounce = document.getElementById("Startb");
 var stop = document.getElementById("stop");
 var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 var frameid;
 
-var draw = function() {
+var bounceC = function() {
+	var radius = 5;
+	var xcor = 250;
+	var ycor = 150;
+	var xchange = 1;
+	var ychange = 1;
+	var animate = function() {
+		//if you reach width limits
+		if (xcor + radius == 500 || xcor - radius == 0){
+			xchange = xchange * -1;
+		}
+		//if you reach height limits
+		if (ycor + radius == 300 || ycor - radius == 0){
+			ychange = ychange * -1;
+		}
+		c.setAttribute( "cx", xcor + xchange );
+		c.setAttribute( "cy", ycor + ychange );
+		c.setAttribute( "r", radius );
+		c.setAttribute( "fill", "red" );
+		c.setAttribute( "stroke", "black" );
+		pic.appendChild(c);
+		xcor += xchange;
+		ycor += ychange;
+	};
+	frameid = setInterval(animate,10);
+}
+var expandC = function() {
 	var radius = 5;
 	var expand = true;
 	var animate = function() {
@@ -39,7 +66,8 @@ var clear = function(e) {
 	e.preventDefault();
 	pic.removeChild(c);
 }
-start.addEventListener("click",draw);
+start.addEventListener("click",expandC);
+startbounce.addEventListener("click",bounceC);
 stop.addEventListener("click",stopdraw);
 clearbutton.addEventListener("click", clear);
 
